e6d555c366226401d54bd84205c384972744fb88
masataka <ar.tec.masa@gmail.com>
2018-05-03 23:16:21 +0900

    fix

--- src/main-component.js
+++ src/main-component.js
@@ -32,7 +32,7 @@ export class MainComponent extends React.Component {
 
           <h3>music</h3>
           <p>二十歳になりかけの僕</p>
-          <iframe src="https://www.youtube.com/embed/s6kh9_tXrvY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/s6kh9_tXrvY" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
           <p><a href="https://www.facebook.com/masataka.shida/posts/728397967353906?notif_id=1519130065671043&notif_t=feedback_reaction_generic&ref=notif">詳細URL</a></p>
 
           <h3>Prototype</h3>
@@ -41,17 +41,18 @@ export class MainComponent extends React.Component {
           <p>S-StoryBook</p>
           <p>-開発中</p>
           <p>AD変換</p>
-          <iframe src="https://www.youtube.com/embed/SUEkDbkZ4pc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <p>自動運転アルゴリズム</p>
+          <iframe src="https://www.youtube.com/embed/SUEkDbkZ4pc" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <p>アームクローラー 走行テスト</p>
-          <iframe src="https://www.youtube.com/embed/wTjc6jlNvQE" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/wTjc6jlNvQE" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <p>ブロック崩しゲーム</p>
-          <iframe src="https://www.youtube.com/embed/pJEbenRZMSo" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/pJEbenRZMSo" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <p>トグル動作によるLED</p>
-          <iframe src="https://www.youtube.com/embed/C2C98kDc6Yw" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/C2C98kDc6Yw" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <p>タクトスイッチによるLEDの点灯</p>
-          <iframe src="https://www.youtube.com/embed/-tQP6dp0Jqs" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/-tQP6dp0Jqs" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <p>タクトスイッチで音声再生</p>
-          <iframe src="https://www.youtube.com/embed/36E_p7ASqA8" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+          <iframe src="https://www.youtube.com/embed/36E_p7ASqA8" frameBorder="0" allow="encrypted-media" allowFullScreen></iframe>
           <h3>Skill</h3>
           <a href="#top">↑トップへ</a>
         </main>
