b74834c9241454b8192c830bd71b511b0dd363db
masataka <ar.tec.masa@gmail.com>
2018-05-08 09:47:04 +0900

    webpackのコンフィグファイルを変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const MODE = 'production';
+//const MODE = 'development';
+const enabledSourceMap = (MODE === 'development');
+
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'development',
+  mode: MODE,
 
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: './src/main.js',
@@ -36,7 +40,37 @@ module.exports = {
         ],
         // node_modules は除外する
         exclude: /node_modules/,
-      }
+      },
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+ 
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
     ]
   }
 };
