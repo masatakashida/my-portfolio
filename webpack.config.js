7434be6cc917a4fcda59dea58b7e3fb8d7766f14
masataka <ar.tec.masa@gmail.com>
2018-07-04 17:35:19 +0900

    不要なコメントアウトを削除した

--- webpack.config.js
+++ webpack.config.js
@@ -3,69 +3,44 @@ const MODE = 'production';
 const enabledSourceMap = (MODE === 'development');
 
 module.exports = {
-  // モード値を production に設定すると最適化された状態で、
-  // development に設定するとソースマップ有効でJSファイルが出力される
   mode: MODE,
-
-  // メインとなるJavaScriptファイル（エントリーポイント）
   entry: './src/main.js',
-  // ファイルの出力設定
   output: {
-    //  出力ファイルのディレクトリ名
     path: `${__dirname}/dist`,
-    // 出力ファイル名
     filename: 'main.js'
   },
   module: {
     rules: [
       {
-        // 拡張子 .js の場合
         test: /\.js$/,
         use: [
           {
-            // Babel を利用する
             loader: 'babel-loader',
-            // Babel のオプションを指定する
             options: {
               presets: [
-                // env を指定することで、ES2017 を ES5 に変換。
-                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
-                // webpack の Tree Shaking 機能が使えない
                 ['env', {'modules': false}],
-                // React の JSX を解釈
                 'react'
               ]
             }
           }
         ],
-        // node_modules は除外する
         exclude: /node_modules/,
       },
-      // Sassファイルの読み込みとコンパイル
       {
-        test: /\.scss/, // 対象となるファイルの拡張子
+        test: /\.scss/,
         use: [
-          // linkタグに出力する機能
           'style-loader',
-          // CSSをバンドルするための機能
           {
             loader: 'css-loader',
             options: {
-              // オプションでCSS内のurl()メソッドの取り込みを禁止する
               url: false,
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap,
- 
-              // 0 => no loaders (default);
-              // 1 => postcss-loader;
-              // 2 => postcss-loader, sass-loader
               importLoaders: 2
             },
           },
           {
             loader: 'sass-loader',
             options: {
-              // ソースマップの利用有無
               sourceMap: enabledSourceMap,
             }
           }
